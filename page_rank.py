dec60fc3324ba516d174f90a3a5bd76e2cd3c709
Brandon Haas <bhaas412@users.noreply.github.com>
2018-01-31 20:12:30 -0500

    Fixed

--- page_rank.py
+++ page_rank.py
@@ -19,12 +19,12 @@ with open("hollins.dat", "r") as data:
     incoming_links = [set() for x in range(number_of_nodes)]
     outgoing_links = [0 for x in range(number_of_nodes)]
 
-    for x in range(number_of_nodes) :
+    for x in range(number_of_nodes):
         line = next(read)
         index = int(line[0])
         links[index] = line[1]
 
-    for x in range(number_of_edges) :
+    for x in range(number_of_edges):
         line = next(read)
         source_link = int(line[0])
         destination_link = int(line[1])
@@ -32,22 +32,22 @@ with open("hollins.dat", "r") as data:
         outgoing_links[source_link - 1] += 1
         incoming_links[destination_link - 1].add(source_link - 1)
 
-    for x in range(number_of_nodes) :
+    for x in range(number_of_nodes):
         initial_state_vector.append(1 / number_of_nodes)
 
-    for i in range(number_of_nodes) :
-        for j in incoming_links[i] :
+    for i in range(number_of_nodes):
+        for j in incoming_links[i]:
             transition_matrix[i][j] = 1 / outgoing_links[j]
 
-def page_rank(trans_matrix, init_state_vector, damp, inc_links) :
+def page_rank(trans_matrix, init_state_vector, damp, inc_links):
     iterations = 0
     length = len(init_state_vector)
     next_state_vector = [0] * length
 
     while iterations < 100:	# iterate 100 times to achieve final PageRank
-        for i in range(length) :
+        for i in range(length):
             temp_next_state_vector = 0
-            for j in inc_links[i] :
+            for j in inc_links[i]:
                 temp_next_state_vector += init_state_vector[j] * trans_matrix[i][j]
             next_state_vector[i] = (1 - damp) + (damp * temp_next_state_vector)
         init_state_vector = next_state_vector
@@ -58,15 +58,15 @@ def page_rank(trans_matrix, init_state_vector, damp, inc_links) :
 final_rankings = page_rank(transition_matrix, initial_state_vector, damping, incoming_links)
 
 # sort and write final_rankings to text file
-with open("final_rankings.txt", "w") as file :
+with open("final_rankings.txt", "w") as file:
     sorted_rankings = []
 
-    for i in range(len(final_rankings)) :
+    for i in range(len(final_rankings)):
         sorted_rankings.append((final_rankings[i], i))
     sorted_rankings.sort()
     sorted_rankings.reverse()
 
-    for i in sorted_rankings :
+    for i in sorted_rankings:
         rank = i[0]
         index = i[1]
         line = str(index + 1) + ' ' + str(rank) + ' ' + links[index + 1] + '\n'
